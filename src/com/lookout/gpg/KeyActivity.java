cbf883d3b5bbba1a98b323b796692a089404446d
Abhi Yerra <abhi.yerra@lookout.com>
2013-05-29 14:39:12 -0700

    List the potential keys

--- src/com/lookout/gpg/KeyActivity.java
+++ src/com/lookout/gpg/KeyActivity.java
@@ -9,9 +9,12 @@ import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,10 +28,12 @@ public class KeyActivity extends ListActivity implements NfcAdapter.CreateNdefMe
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main);
 
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
-        KeyListAdapter adapter = new KeyListAdapter(this, values);
+        ArrayList<Map<String, String>> list = buildData();
+        String[] from = { "full_name", "pgp_fingerprint" };
+        int[] to = { R.id.full_name, R.id.pgp_fingerprint };
+
+        SimpleAdapter adapter = new SimpleAdapter(this, list,
+                R.layout.key_list_item, from, to);
         setListAdapter(adapter);
 
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
@@ -39,6 +44,22 @@ public class KeyActivity extends ListActivity implements NfcAdapter.CreateNdefMe
         }
         // Register callback
         mNfcAdapter.setNdefPushMessageCallback(this,this);
+    }
+
+    private ArrayList<Map<String, String>> buildData() {
+        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        list.add(putData("Abhi Yerra", "1024D/EC92C369", "1"));
+        list.add(putData("Shane Wilton", "1024D/02C834B6", "2"));
+        list.add(putData("Derek Halliday", "1024D/33D8457A", "3"));
+        return list;
+    }
+
+    private HashMap<String, String> putData(String name, String pgp_fingerprint, String trust_level) {
+        HashMap<String, String> item = new HashMap<String, String>();
+        item.put("full_name", name);
+        item.put("pgp_fingerprint", pgp_fingerprint);
+        item.put("trust_level", trust_level);
+        return item;
     }
 
     @Override
