e2ad83f4f4ea99f8e2b81582a0a3e52eeeee5265
Shane Wilton <shane.wilton@lookout.com>
2013-05-29 21:02:09 -0700

    Start of the GPGCli bindings.

--- src/com/lookout/gpg/GPGCli.java
+++ src/com/lookout/gpg/GPGCli.java
@@ -1,5 +1,7 @@
 package com.lookout.gpg;
 
+import android.util.Log;
+
 import java.io.*;
 import java.util.ArrayList;
 
@@ -7,38 +9,53 @@ public class GPGCli implements GPGBinding {
 
     private final String GPG_PATH = "/data/data/info.guardianproject.gpg/app_opt/aliases/gpg2";
 
-    public GPGCli() throws IOException {
-        String[] arguments = {"--version"};
-        Runtime.getRuntime().exec(GPG_PATH, arguments);
+    public GPGCli() {
+        Log.i("LookoutPG", "GPGCli initialized");
     }
 
-    public void ImportKey(String keyFile) throws IOException {
-        String[] arguments = {"--import", keyFile};
-        Runtime.getRuntime().exec(GPG_PATH, arguments);
+    public void ImportKey(String keyFile) {
+        Exec(GPG_PATH, "--yes", "--import", keyFile);
+
+        Log.i("LookoutPG", keyFile + " imported");
     }
 
-    public String ExportAsAsciiArmor(PublicKey key) throws IOException {
-        String[] arguments = {"--armor", "--export", key.GetKeyId()};
-        Process p = Runtime.getRuntime().exec(GPG_PATH, arguments);
+    public String ExportAsAsciiArmor(String keyId) {
+        String output = Exec(GPG_PATH, "--armor", "--export", keyId);
+        Log.i("LookoutPG", keyId + " exported");
 
-        return getProcessOutput(p);
+        return output;
     }
 
-    public ArrayList<PublicKey> GetKeys() throws IOException {
-        String[] arguments = {"--list-keys"};
-        Process p = Runtime.getRuntime().exec(GPG_PATH, arguments);
+    public ArrayList<PublicKey> GetKeys() {
+        String output = Exec(GPG_PATH, "--list-keys");
+        Log.i("LookoutPG", "Got keys: " + output);
 
-        throw new IOException();
+        return new ArrayList<PublicKey>();
+    }
+
+    private String Exec(String... command) {
+        String rawOutput = "";
+        try {
+            Process p = new ProcessBuilder(command).redirectErrorStream(true).start();
+            p.waitFor();
+            rawOutput = getProcessOutput(p);
+        } catch(IOException e) {
+
+        } catch (InterruptedException e) {
+
+        }
+        return rawOutput;
     }
 
     private String getProcessOutput(Process p) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        StringBuilder s = new StringBuilder();
+        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        StringBuilder sb = new StringBuilder();
         String line;
-        while((line = bf.readLine()) != null) {
-            s.append(line);
+        while ((line = input.readLine()) != null) {
+            sb.append(line + "\n");
         }
+        input.close();
 
-        return s.toString();
+        return sb.toString();
     }
 }
