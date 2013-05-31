95f9d788faeb5b631e13a3b787cd3d769ab522b7
Abhi Yerra <abhi.yerra@lookout.com>
2013-05-31 03:49:47 -0700

    testing changes. we should revert this

--- src/com/lookout/keymaster/MainActivity.java
+++ src/com/lookout/keymaster/MainActivity.java
@@ -70,7 +70,9 @@ public class MainActivity extends Activity  implements NfcAdapter.CreateNdefMess
                 "Exchange Keys",
                 "Sync Keyring",
                 "Public Keys",
-                "Private Keys"
+                "Private Keys",
+                "READY TO VERIFY",
+                "SERVER PUSH"
         };
 
         MenuArrayAdapter adapter2 = new MenuArrayAdapter(this, values);
@@ -105,10 +107,48 @@ public class MainActivity extends Activity  implements NfcAdapter.CreateNdefMess
                     loadFragment(new KeyFragment());
                     break;
                 case 5:       // Only for TESTING.
+
+                    String fakeKey =
+                            "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+                                    "Version: GnuPG v1.4.13 (Darwin)\n" +
+                                    "Comment: GPGTools - http://gpgtools.org\n" +
+                                    "\n" +
+                                    "mQENBFGn314BCAC0pa26XqsEUAwX3WQaReD6WpZauEPsWoefAlLaX43F8eS1QLyG\n" +
+                                    "art+n5IBb5OcnZpDIl/qPlct02wu/bf5eLOe3OfGjgISB27Yq01OYEwQJ7cYVWIg\n" +
+                                    "+C79tI1ApoXlqj1Dai7nyNCJS444PF1o3Vj9ILNZXAlfSIxlPG6bgdsLTLm0SB2Z\n" +
+                                    "J1BC6bBLE38wKBuu7/wGhHG9+euWratnh24dCqYj3opImnmaFQen7znUUNx7XSOR\n" +
+                                    "BQUpuUPS8cYgx6hLTc2SkC0FfKnESSeVGEfFtGQL1dLmSpw4zZrhk5rYs3LQ5ZBN\n" +
+                                    "X9/1WMLWG1EqV/pdN/LCyttVdgBBnEd8E69vABEBAAG0JlRlc3QgTG9va291dCA8\n" +
+                                    "dGVzdGxvb2tvdXRAZXhhbXBsZS5jb20+iQE+BBMBAgAoBQJRp99eAhsDBQkADS8A\n" +
+                                    "BgsJCAcDAgYVCAIJCgsEFgIDAQIeAQIXgAAKCRC26aFtFfrpb+6BCACDbX716RXi\n" +
+                                    "9LUk/LJW6onk+ASMoJE/3MYCvzJ476PDQ0CX6ZXpdLijn7TBzpOxYmgKfF+DdV1t\n" +
+                                    "svd6JN6VGCZi7B6IxZZ2tLSnYmyLXLc+ZrFU9+iYKUqWjeuVHDNv2m2Vtj9IP4QW\n" +
+                                    "vwkslrhoJZimUkwph0YXzmU3UvyXYs06HSykqTS+5V85eHs7MZZi/5yqo3/Zn3yE\n" +
+                                    "32E5ViDtU4uChAkKvNkx4tCLO/Watv1wPLIvNYNLgGdkq7IGOSoHo+GWRUqrgGxa\n" +
+                                    "p2kMZbDnSAA7Uml8AfF3ALyGtMmHIdyGzRZIGUcqN8JXmz54qDwXEDzf8BO+bHYg\n" +
+                                    "e4Cfd+ak1GgauQENBFGn314BCADQLZ0+vtZuzYqO8vtS8/dziTH0yntxglqZ0EAR\n" +
+                                    "R7u1jiGaKdlgRmjQVFg+vffPmjnSKA63DDOz3Bu+SAODXAiuEiSlSR4oAJ02PPDS\n" +
+                                    "FtdBvNE6C62ids1W+jrif74OWoU2+44bYqML8YCuy0lWMQSMWQZdcbH6K4S9IxCr\n" +
+                                    "ooWFAYcweWhb0rAgYj4TjdayV5Pdhd9zS2V6uqcNkZxNmJ54huDSZ4mILmi7XO4s\n" +
+                                    "GYvFSuycYNRLxMyYpUcityjc0mP9Zo9zixQE5CL5yn6LfrtIG/+gsCaKBzIOIhqZ\n" +
+                                    "WyDEJFclwqgr8YsWbw7QheRmdmXQCUa14UzfyyBC1wdx425xABEBAAGJASUEGAEC\n" +
+                                    "AA8FAlGn314CGwwFCQANLwAACgkQtumhbRX66W8FLQgAlgrQoGUb5xAT1cqbmzw/\n" +
+                                    "x/v1MiU24MdP4qan0qqGExnHQ7u+h6xwX4in8Iv1fdEwPRyC7M9xfmx2N7K3L9o2\n" +
+                                    "KrgEbOlKpIINGMe4F1vSY+66VlPV0oUrimIhZ71uO3so5+84t3pHdMTP7h4qUEJI\n" +
+                                    "IQ5r9JXHY8ett/9G4hDlEvPyR5FS5erEaHbmAPQY9bxVnWbThbHWBwkl7PHcsvjk\n" +
+                                    "zP6YDzT1GCT8Z+vyEUfOUN+jjsKaK0qfMBCjkQyXFduJavj7FY1K5SdI85IIUBA0\n" +
+                                    "uCo4Stm/iZQ1Q0ORWn/gCJbImBrygu4FHpX9Vg8qDZBMqEPWotdBTQILXjqGbcdk\n" +
+                                    "Ag==\n" +
+                                    "=cptb\n" +
+                                    "-----END PGP PUBLIC KEY BLOCK-----";
+
+                    GPGFactory.setPublicKey("67280F23EC92C369");
+                    GPGFactory.setReceivedKey(fakeKey, "B6E9A16D15FAE96F");
                     loadFragment(new KeyVerifyFragment());
                     break;
                 case 6:
                     loadFragment(new ServerPushFragment());
+                    break;
             }
 
 
